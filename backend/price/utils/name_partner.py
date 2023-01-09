03f4f750edc6de245b45bae0a65e9125c5f0476a
DenisDemenev <lazer89@list.ru>
2023-01-09 23:22:27 +0500

    Навигация по истории

--- backend/price/utils/name_partner.py
+++ backend/price/utils/name_partner.py
@@ -1,6 +1,4 @@
 import os
-
-
 import vk
 
 token = os.environ.get('VK_TOKEN')
@@ -16,12 +14,11 @@ def groups(id):
     return api.groups.getById(group_id=id, fields='members_count')[0]
 
 
-
 def coverage(id):
     api = vk.API(access_token=token, v='5.131')
     data = api.wall.get(domain=id, offset=10, count=20)['items']
     count = 0
-    for i in range(len(data)): 
+    for i in range(len(data)):
         post = data[i]
         count += post['views']['count']
     return count / len(data)
