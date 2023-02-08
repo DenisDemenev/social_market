627b192f889ac4031a5fb9bf4175908b552fc912
DenisDemenev <lazer89@list.ru>
2023-02-08 22:08:41 +0500

    cart and favorite

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -10,6 +10,19 @@ class GroupSearchFilter(SearchFilter):
 
 class SubjectFilterVk(FilterSet):
     subject = filters.AllValuesMultipleFilter(field_name='subject_slug')
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(cart__user=self.request.user)
+        return queryset
 
     class Meta:
         model = Groups
