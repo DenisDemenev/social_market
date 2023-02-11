fcb28802ae7c67a1ad5848217f61424dc3a2d628
DenisDemenev <lazer89@list.ru>
2023-02-11 12:40:23 +0500

    basket

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -11,7 +11,7 @@ token = os.environ.get('VK_TOKEN_GROUPS')
 
 def order_shopping_cart(user):
     api = vk.API(access_token=token, v='5.131')
-    groups = Cart.objects.all()
+    groups = list(Cart.objects.all())
     api.messages.send(user_id=773837067,
                       random_id=100, message=f'{user} {groups}')
     return HttpResponse(content_type='application/json')
