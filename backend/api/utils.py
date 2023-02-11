bed1c092433ebe9e4172fc11f61eea75c9559fae
DenisDemenev <lazer89@list.ru>
2023-02-11 13:17:51 +0500

    basket

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -4,14 +4,14 @@ from django.http import HttpResponse
 
 import vk
 
-# from basket.models import Cart
+from basket.models import Cart
 
 token = os.environ.get('VK_TOKEN_GROUPS')
 
 
 def order_shopping_cart(user):
     api = vk.API(access_token=token, v='5.131')
-    # groups = Cart.objects.filter(user=user)
+    groups = Cart.objects.filter(user=user)
     api.messages.send(user_id=773837067,
-                      random_id=100, message=user)
+                      random_id=0, message=groups)
     return HttpResponse(content_type='application/json')
