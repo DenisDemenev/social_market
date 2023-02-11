707df8185b2f64ad3a1f65e6fd09a5fb3e124822
DenisDemenev <lazer89@list.ru>
2023-02-11 17:42:07 +0500

    basket

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -13,12 +13,10 @@ token = os.environ.get('VK_TOKEN_GROUPS')
 def order_shopping_cart(user):
     api = vk.API(access_token=token, v='5.131')
     groups = Cart.objects.filter(user=user)
-    price = Cart.objects.filter(user=user).annotate(Sum('price'))
     message = f"""
                Пользователь: {user}.
                Заказал:
                {groups}
-               Итого: {price}
                """
     api.messages.send(user_id=773837067,
                       random_id=0, message=message)
