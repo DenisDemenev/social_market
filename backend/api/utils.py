a2cab41e10af1cbac17204ed1a3d8a5a56e0bf99
DenisDemenev <lazer89@list.ru>
2023-02-14 18:03:51 +0500

    message confirm

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -12,19 +12,19 @@ token = os.environ.get('VK_TOKEN_GROUPS')
 
 def order_shopping_cart(user):
     api = vk.API(access_token=token, v='5.131')
-    groups = Cart.objects.filter(user=user).values('group_vk__name', 'group_vk__link', 'group_vk__price')
+    groups = Cart.objects.filter(user=user).values(
+        'group_vk__name', 'group_vk__link', 'group_vk__price')
     sum = 0
     for i in range(len(groups)):
         sum += groups[i]['group_vk__price']
-    group_message=[]
+    group_message = []
     for idx, grp in enumerate(groups, start=1):
         group_message.append((
             f"""
                {idx}. {grp["group_vk__name"]} - {grp["group_vk__price"]} руб 
                {grp["group_vk__link"]}
              """
-        )
-    )
+        ))
     user_social_uid = ''
     if UserSocialAuth.objects.filter(user=user):
         user_social_uid = UserSocialAuth.objects.filter(
