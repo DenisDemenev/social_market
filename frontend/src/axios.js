0683f2376d3e878255d4cca7196e8fe3e138a184
DenisDemenev <lazer89@list.ru>
2023-07-18 17:30:27 +0500

    test

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: 'https://smax.store/api',
   // baseURL: "http://localhost:8000/api",
-  baseURL: "http://80.87.96.10:8000/api",
+  baseURL: "http://80.87.96.10/api",
 });
 
 if (localStorage.getItem("token")) {
