42a431be73c3d7dc1a3812498255c7aa8d73db2c
DenisDemenev <lazer89@list.ru>
2023-07-18 22:58:59 +0500

    Костыли авторизации ВК

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
-    # path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.social.urls')),
 ]
