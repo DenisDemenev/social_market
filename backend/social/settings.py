4f5687da1e5b86425a8ca9afbba74092b88b8835
DenisDemenev <lazer89@list.ru>
2023-08-01 20:31:54 +0500

    test

--- backend/social/settings.py
+++ backend/social/settings.py
@@ -175,6 +175,3 @@ DJOSER = {
         'user_list': ('rest_framework.permissions.AllowAny',)
     }
 }
-
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
