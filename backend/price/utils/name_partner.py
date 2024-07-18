44fdd5a60a1cf2173f2a6ffa713057fe4544d3d0
DenisDemenev <lazer89@list.ru>
2024-07-18 23:11:03 +0500

    test

--- backend/price/utils/name_partner.py
+++ backend/price/utils/name_partner.py
@@ -4,22 +4,23 @@ import vk
 
 token = os.environ.get('VK_TOKEN')
 
-
 def partner(id):
-    api = vk.API(access_token=token, v='5.131')
+    api = vk.API(access_token=token, v='5.139')
     return api.users.get(user_ids=id, fields='photo_max')[0]
 
 
 def groups(id):
-    api = vk.API(access_token=token, v='5.131')
-    return api.groups.getById(group_id=id, fields='members_count')[0][0]
+    api = vk.API(access_token=token, v='5.139')
+    return api.groups.getById(group_id=id, fields='members_count')['groups'][0]
 
 
 def coverage(id):
-    api = vk.API(access_token=token, v='5.131')
+    api = vk.API(access_token=token, v='5.139')
     data = api.wall.get(domain=id, offset=10, count=20)['items']
     count = 0
     for i in range(len(data)):
         post = data[i]
         count += post['views']['count']
     return count / len(data)
+
+print (groups(544555))
